ad9bd7183be450fe07ffce65b0213bf10e27d73f
akcharlie24 <akshatbhardwaj24@gmail.com>
2023-09-20 12:36:44 +0530

    issue resolved

---
+++ LB/basics.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int a = 7;
+    cout << "Size of a is " << sizeof(a) << endl; // sizeof operator
+ 
+    // char ch = 256; // this throws a warning as
+    // cout << "The value of char ch is " << ch << endl;
+    return 0;
+}
