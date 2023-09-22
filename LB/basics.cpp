6b3328a8e338d28610ecd4c604f8b4d538a4695d
akcharlie24 <akshatbhardwaj24@gmail.com>
2023-09-22 06:45:48 +0530

    modified basic.cpp

--- LB/basics.cpp
+++ LB/basics.cpp
@@ -6,7 +6,9 @@ int main()
     int a = 7;
     cout << "Size of a is " << sizeof(a) << endl; // sizeof operator
  
-    // char ch = 256; // this throws a warning as
-    // cout << "The value of char ch is " << ch << endl;
+    // char ch = 256; // this throws a warning as 256 is out of range of char data type 
+    // cout << "The value of char ch is " << ch << endl; // and hence the output is garbage value
+    
+    
     return 0;
 }
