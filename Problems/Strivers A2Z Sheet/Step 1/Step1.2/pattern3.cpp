dfeca5eb222f31e389d35dab06b9bef048b89c26
akcharlie24 <akshatbhardwaj24@gmail.com>
2023-09-30 13:22:23 +0530

    changes made

---
+++ Problems/Strivers A2Z Sheet/Step 1/Step1.2/pattern3.cpp
@@ -0,0 +1,7 @@
+//in pattern 2 do the following :
+
+//replace cout << "* " 
+// with  
+//cout << j ; 
+
+//inside the j wala for loop to print numbers
---
+++ Problems/Strivers A2Z Sheet/Step 1/Step1.2/pattern4.cpp
@@ -0,0 +1,7 @@
+//in pattern 2 do the following :
+
+//replace cout << "* " 
+// with  
+//cout << i ; // jo bhi i ki value hogi uss 
+
+//inside the j wala for loop to print numbers
