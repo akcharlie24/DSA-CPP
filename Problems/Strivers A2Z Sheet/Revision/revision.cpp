7d1c7531c5ceb261d452f85337f3e9bc53f8ca3e
akcharlie24 <akshatbhardwaj24@gmail.com>
2023-11-25 10:23:41 +0530

    changes made

--- Problems/Strivers A2Z Sheet/Revision/revision.cpp
+++ Problems/Strivers A2Z Sheet/Revision/revision.cpp
@@ -1,7 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+void pattern (int n){
+    for (int i = n; i < 0; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
     
+}
+
+int main() {
+    int n = 10;
+    pattern(n);
     return 0;
 }
