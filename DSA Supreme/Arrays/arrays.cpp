67d834b1d428af635fcb839afd885f3b84fd027d
akcharlie24 <akshatbhardwaj24@gmail.com>
2023-12-10 22:57:21 +0530

    changes made

---
+++ DSA Supreme/Arrays/arrays.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    //initialization in an array    
+    int arr[10] = {1,2,3,4,5};// remaining elements are initialized to 0
+    //int arr2[4] = {1,2,3,4,3};
+    return 0;
+}
