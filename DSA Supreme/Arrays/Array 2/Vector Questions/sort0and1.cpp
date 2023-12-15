dd21f65f819f5cb5d7956e9fa8507580b33847bf
akcharlie24 <akshatbhardwaj24@gmail.com>
2023-12-15 19:43:31 +0530

    changes made

--- DSA Supreme/Arrays/Array 2/Vector Questions/sort0and1.cpp
+++ DSA Supreme/Arrays/Array 2/Vector Questions/sort0and1.cpp
@@ -1,1 +1,32 @@
-//revisit the 44. Array 2 in DSA Supreme to see the code for this.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr{0, 1, 0, 1, 0, 1, 1, 1};
+
+    int start = 0;
+    int end = arr.size() - 1;
+    int i = 0;
+
+    while (i != end)
+    {
+        if (arr[i] == 0)
+        {
+            swap(arr[i], arr[start]);
+            start++;
+            i++;
+        }
+        else if (arr[i] == 1)
+        {
+            swap(arr[i], arr[end]);
+            end--;
+        }
+    }
+
+    for (auto value : arr)
+    {
+        cout << value << " ";
+    }
+    return 0;
+}
