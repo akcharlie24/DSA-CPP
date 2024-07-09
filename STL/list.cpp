dc94231140f265a4b0b81922a9f4eb342ce93cef
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-07-09 13:14:14 +0530

    changes made

--- STL/list.cpp
+++ STL/list.cpp
@@ -1,13 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    list<int> l1 = {1, 2, 3, 4, 5}; 
+int main()
+{
+    list<int> l1 = {1, 2, 3, 4, 5};
 
     l1.push_front(10); // {10, 1, 2, 3, 4, 5}
-    l1.pop_front(); // {1, 2, 3, 4, 5} 
+    l1.pop_front();    // {1, 2, 3, 4, 5}
 
-    //list just gives us front operations also 
-    //rest operations are same as vectors 
+    // list just gives us front operations also
+    // rest operations are same as vectors
     return 0;
 }
