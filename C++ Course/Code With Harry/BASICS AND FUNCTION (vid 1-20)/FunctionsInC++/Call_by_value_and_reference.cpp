3d77cfa4c1997aabdd4c53a8e2c2eca30a4e4515
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-07-10 15:04:24 +0530

    managed folders

--- C++ Course/Code With Harry/BASICS AND FUNCTION (vid 1-20)/FunctionsInC++/Call_by_value_and_reference.cpp
+++ C++ Course/Code With Harry/BASICS AND FUNCTION (vid 1-20)/FunctionsInC++/Call_by_value_and_reference.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
 using namespace std;
 
-void swap(int c, int d){  // This wont swap the numbers
+void swap(int c, int d)
+{ // This wont swap the numbers
     int Temp = c;
-    c=d;
-    d=Temp;
+    c = d;
+    d = Temp;
 }
 
-void swapPointer(int* a, int* b){
+void swapPointer(int *a, int *b)
+{
     int temp = *a;
-    *a=*b;
-    *b=temp;
+    *a = *b;
+    *b = temp;
 }
 
-void swapReferenceVar(int &a, int &b){ // This is known as call by reference (as reference de dia)
+void swapReferenceVar(int &a, int &b)
+{ // This is known as call by reference (as reference de dia)
     // a and b are reference variables that are aliases for x and y // matlab a and b are just nicknames for x and y
     int temp = a;
-    a=b;
-    b=temp;
+    a = b;
+    b = temp;
 }
 
-int main () {
-    int x=4,y=5;                
+int main()
+{
+    int x = 4, y = 5;
 
-    cout<<"The numbers before swap are "<<x<<" and "<<y<<endl;
-    
-    // swap(x,y); // This wont swap the numbers (explained in copy) 
-    
-    swapPointer(&x , &y); //As humne formal parameters pointers bnae hai to hume address pass krane padenge  
-                          //This is known as call by reference (as reference de dia)
+    cout << "The numbers before swap are " << x << " and " << y << endl;
 
-    cout<<"The numbers after swapping are "<<x<<" and "<<y;
+    // swap(x,y); // This wont swap the numbers (explained in copy)
+
+    swapPointer(&x, &y); // As humne formal parameters pointers bnae hai to hume address pass krane padenge
+                         // This is known as call by reference (as reference de dia)
+
+    // swapReferenceVar(x,y);
+
+    cout << "The numbers after swapping are " << x << " and " << y << endl;
     return 0;
 }
